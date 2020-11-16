3f3da40c26f9e478b01aff123941ca2727be3ea9
lucasoskorep <lucas.oskorep@gmail.com>
2020-11-16 17:03:13 -0600

    Adding in files needed by default

--- app/src/main/java/com/example/bts_todo_list_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/bts_todo_list_kotlin/MainActivity.kt
@@ -1,12 +1,8 @@
 package com.example.bts_todo_list_kotlin
 
-import android.content.DialogInterface
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.widget.EditText
 import android.widget.ListView
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.gson.Gson
@@ -18,45 +14,25 @@ class MainActivity : AppCompatActivity() {
     private lateinit var listView: ListView
     private lateinit var dataList: ArrayList<ListItem>
     private lateinit var floatingActionButton: FloatingActionButton
-    private lateinit var customArrayAdapter:CustomListItemAdapter
+//    private lateinit var customArrayAdapter:CustomListItemAdapter
     private var gson = Gson()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         resources.openRawResource(R.raw.test).bufferedReader().use {
             val stype = object : TypeToken<ArrayList<ListItem>>() {}.type
             dataList = gson.fromJson(it.readText(), stype)
         }
-        //find view objects
-        listView = findViewById(R.id.list_view)
-        floatingActionButton = findViewById(R.id.floatingActionButton)
-        floatingActionButton.setOnClickListener { view -> addNewNote(view) }
-
         //create list data
         dataList.forEach { item ->
             Log.d(TAG, item.toString())
         }
+        //find view objects
+        listView = findViewById(R.id.list_view)
         //hook list data up to our list using array adapter
-        customArrayAdapter = CustomListItemAdapter(this, dataList)
-        listView.adapter = customArrayAdapter
     }
 
-    fun addNewNote(view: View) {
-        val builder: android.app.AlertDialog.Builder = android.app.AlertDialog.Builder(this)
-        builder.setTitle("Name")
-        val customLayout: View = layoutInflater.inflate(R.layout.custom_alert_dialog, null)
-        builder.setView(customLayout)
-        builder.setPositiveButton("OK") { _, _ ->
-                // AlertDialog to the Activity
-                val number = customLayout.findViewById<EditText>(R.id.number).text.toString()
-                val title = customLayout.findViewById<EditText>(R.id.title).text.toString()
-                val description = customLayout.findViewById<EditText>(R.id.description).text.toString()
-                dataList.add(ListItem(title, description, number.toInt()))
-                customArrayAdapter.notifyDataSetChanged()
-            }
-        val dialog: android.app.AlertDialog? = builder.create()
-        dialog?.show()
-    }
 
 }
